2a0a58e075bf4a618b362c0b51e4f54a99487ced
Sushant Goswami <49872047+c188578@users.noreply.github.com>
2021-07-24 01:03:07 +0530

    Update namp_integrity_check.sh

--- namp_integrity_check.sh
+++ namp_integrity_check.sh
@@ -15,52 +15,65 @@ PRIMARY_EMAIL=Tcs_Platform_Linux@lists.lilly.com
 SECONDARY_EMAIL=ranjan_alok@network.lilly.com
 PRIMARY_MAIL_ENABLE=1
 SECONDARY_MAIL_ENABLE=1
-SIZE_LIMIT=512
-WORKDIR=/etc/msudoers/updsudo_shadow
-UPDSUDO_WORKDIR=/etc/msudoers
-AUTO_BACKUP_DIR=backup
-SUDO_FILE=master.sudoers
+WORKDIR=/var
 LOGDIR=log
-LOGFILE=updsudo_shadow.log
+LOGFILE=namp_integrity_check_log.txt
 TEMPDIR=tmp
-CHANGE_DIR=changes
-UPDSUDO_HOUR_CHECK=01
 ############## Pre Fixed Variables ##############################
 CURRENTDATE=`date | awk '{print $3"-"$2"-"$6}'`
 CURRENTTIMESTAMP=`date | awk '{print $4}' | sed '$ s/:/./g'`
-LAST_FILE_DATE=""
-LAST_FILE_HOUR=""
-LAST_FILE_DATE=`ls -ltr $WORKDIR/$AUTO_BACKUP_DIR | tail -1 | awk '{print $9}' | cut -d "-" -f 2,3,4`
-LAST_FILE_HOUR=`ls -ltr $WORKDIR/$AUTO_BACKUP_DIR | tail -1 | awk '{print $9}' | cut -d "-" -f 5 | cut -d "." -f 1`
-CURRENT_HOUR=`date | awk '{print $4}' | cut -d ":" -f 1`
-COPY_FLAG=0
 ###################### Help Menu ##########################################
 if [ -z $1 ]; then
  echo "(MSG 000): No arguments passed, continuing to regular task" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
 else
  if [ $1 == "-help" ]; then
-  echo "(MSG HELP): The script is intended to check the master sudoers file on regular interval, it is needed to set it by cron"
+  echo "(MSG 001 HELP): The script is intended to check the namp database integrity with active directory database, it is needed to set it by cron"
   exit 0;
  fi
 fi
 #################### Do not edit below this line, use variables above ###########################################
 ###------------------------------------------------------------------------------------------------------------###
+
 ########################## Duplicate instance check ######################################
 CHECK_ID=""
-CHECK_ID=`ps -ef | grep "updsudo_shadow" | grep -v grep | grep -v tail | wc -l`
+CHECK_ID=`ps -ef | grep "namp_integrity_check" | grep -v grep | grep -v tail | wc -l`
 if [ $CHECK_ID -gt 4 ]; then
- echo "(MSG 001): Another intance of updsudo_shadow is running on background, please check and terminate the first session" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
+ echo "(MSG 002): Another intance of updsudo_shadow is running on background, please check and terminate the first session" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
  if [ $SECONDARY_MAIL_ENABLE == 1 ]; then
-  echo "(MSG 001): Another intance of updsudo_shadow is running on background, please check and terminate the first session" | mailx -s "Alert on $ZONE Master Sudo Server" $SECONDARY_EMAIL
+  echo "(MSG 002): Another intance of updsudo_shadow is running on background, please check and terminate the first session" | mailx -s "Alert on $ZONE Master Sudo Server" $SECONDARY_EMAIL
+ fi
+ if [ $PRIMARY_MAIL_ENABLE == 1 ]; then
+  echo "(MSG 002): Another intance of updsudo_shadow is running on background, please check and terminate the first session" | mailx -s "Alert on $ZONE Master Sudo Server" $PRIMARY_EMAIL
  fi
  exit 0;
 fi
-CHECK_VI=""
-CHECK_VI=`ps -ef | grep "vi updsudo_shadow" | grep -v grep | wc -l`
-if [ $CHECK_VI != 0 ]; then
- echo "(MSG 002): vi sessions are running on the background for updsudo_shadow, please close the existing sessions" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
- if [ $SECONDARY_MAIL_ENABLE == 1 ]; then
-  echo "(MSG 002): vi sessions are running on the background for updsudo_shadow, please close the existing sessions" | mailx -s "Alert on $ZONE Master Sudo Server" $SECONDARY_EMAIL
- fi
- exit 0;
-fi
+########################## Duplicate instance check ######################################
+
+########################## Main code #####################################
+/remote/bin/namp/db/listuid > $WORKDIR/$TEMPDIR/tmp_nampuidlist01.txt
+echo -e "$(sed '1d' $WORKDIR/$TEMPDIR/tmp_nampuidlist01.txt )\n" > $WORKDIR/$TEMPDIR/tmp_nampuidlist02.txt
+tr '[A-Z]' '[a-z]' < $WORKDIR/$TEMPDIR/tmp_nampuidlist02.txt > $WORKDIR/$TEMPDIR/namp_integrity_check-nampuid-$CURRENTDATE.txt
+rm -rf $WORKDIR/$TEMPDIR/tmp_nampuidlist01.txt $WORKDIR/$TEMPDIR/tmp_nampuidlist02.txt
+
+adquery user | awk -F'[:=]' '{print $1 "\t" $3}' > $WORKDIR/$TEMPDIR/tmp_aduidlist01.txt
+tr '[A-Z]' '[a-z]' < $WORKDIR/$TEMPDIR/tmp_aduidlist01.txt > $WORKDIR/$TEMPDIR/namp_integrity_check-aduid-$CURRENTDATE.txt
+rm -rf $WORKDIR/$TEMPDIR/tmp_aduidlist01.txt
+
+while read i;
+ do
+  a=`echo "$i" | awk '{print $1}'`
+  b=`echo "$i" | awk '{print $2}'`
+  c=`grep -i "$a " $WORKDIR/$TEMPDIR/namp_integrity_check-aduid-$CURRENTDATE.txt | wc -l`
+   if [ $c -gt 1 ]; then
+     echo "(MSG 003): more than two records found in AD for user $a in NAMP" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
+   fi
+   if [ $c == 1 ]; then
+    d=`grep -i "$a " $WORKDIR/$TEMPDIR/namp_integrity_check-aduid-$CURRENTDATE.txt | awk '{print $1}'`
+    e=`grep -i "$a " $WORKDIR/$TEMPDIR/namp_integrity_check-aduid-$CURRENTDATE.txt | awk '{print $2}'`
+    if [ $b != $e ]; then
+     echo "(MSG 003): Mismatch for user $a with uid $b in NAMP with id $e in AD" | sed -e "s/^/$(date | awk '{print $3"-"$2"-"$6"-"$4}') /" >> $WORKDIR/$LOGDIR/$LOGFILE
+    fi
+   fi
+ done < $WORKDIR/$TEMPDIR/namp_integrity_check-nampuid-$CURRENTDATE.txt
+
+########################## Main code #####################################
